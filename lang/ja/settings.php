073629df4db1837a2c9fbe6b518239da9d3b6cd2
LefTonbo <dragonbird.tonbo@gmail.com>
2017-04-15 19:39:43 +0900

    Add Japanese language

---
+++ lang/ja/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * English language file
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Giuseppe Di Terlizzi <giuseppe.diterlizzi@gmail.com>
+ */
+
+// for the configuration manager
+$lang['defaultSchemaOrgType'] = 'JSON-LDで使用するデフォルトのSchema.orgタイプ';
+$lang['excludedPages']        = '除外するページ (正規表現で入力)';
+$lang['exposeWebService']     = 'Ajax WebServiceを公開する';
+$lang['useDublinCore']        = 'Dublin Coreメタデータを追加する';
+$lang['useJSONLD']            = 'JSON-LDを追加する';
+$lang['useMetaAuthor']        = 'authorメタタグを追加する';
+$lang['useMetaDescription']   = 'descriptionメタタグを追加する';
+$lang['useOpenGraph']         = 'Open Graph protocolのメタデータを追加する';
